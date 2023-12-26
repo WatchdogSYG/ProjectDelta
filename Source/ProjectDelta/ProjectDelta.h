f822b98da9e15f33aaeb110c0903f05e6fef993d
WatchdogSYG <brandonlu001@gmail.com>
2023-12-26 17:00:55 +1030

    Drawline to centre of selected actor progress

--- Source/ProjectDelta/ProjectDelta.h
+++ Source/ProjectDelta/ProjectDelta.h
@@ -3,3 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+
+//CustomStencil Values that M_PP_Outlines reads from. Must be manually synced with M_PP_Outlines and OutlineColorParameterCollection if the values or number of params need to change.
+#define STENCIL_LOOK_OUTLINE 1;
+#define STENCIL_SELECTED_OUTLINE 2;
+#define STENCIL_ENEMY_OUTLINE 3;
